0a42f8adbb755d58b2d946c83da1b7c86ca2cf01
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-03-12 14:04:33 +0100

    Added ITrack interface and implemented FlightTrack Class

---
+++ AirTraficMonitoring/Track/FlightTrack.cs
@@ -0,0 +1,20 @@
+﻿namespace AirTraficMonitoring.Track
+{
+    public class FlightTrack : ITrack
+    {
+        public FlightTrack(string tag, double xpos, double ypos, double alt)
+        {
+            Tag = tag;
+            XPosition = xpos;
+            YPosition = ypos;
+            Altitude = alt;
+        }
+
+        public string Tag { get; set; }
+        public double XPosition { get; set; }
+        public double YPosition { get; set; }
+        public double Altitude { get; set; }
+        public double Velocity { get; set; }
+        public double CompassCourse { get; set; }
+    }
+}
---
+++ AirTraficMonitoring/Track/ITrack.cs
@@ -0,0 +1,12 @@
+﻿namespace AirTraficMonitoring.Track
+{
+    public interface ITrack
+    {
+        string Tag { get; set; }
+        double XPosition { get; set; }
+        double YPosition { get; set; }
+        double Altitude { get; set; }
+        double Velocity { get; set; }
+        double CompassCourse { get; set; }
+    }
+}
