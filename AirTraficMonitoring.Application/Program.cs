dc200447f58d9253a35b27cbf3bcd199ff6f5e53
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-03-13 11:42:01 +0100

    Added Monitor Interface and implemented ConsoleMonitor

--- AirTraficMonitoring.Application/Program.cs
+++ AirTraficMonitoring.Application/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
 using AirTraficMonitoring.Logger;
+using AirTraficMonitoring.Monitor;
+using AirTraficMonitoring.Track;
 
 namespace AirTraficMonitoring.Application
 {
@@ -8,11 +11,29 @@ namespace AirTraficMonitoring.Application
     {
         static void Main(string[] args)
         {
-            ILog file = new ConsoleLog();
 
-            file.LogInformation("");
+            var Flights = new List<ITrack>()
+            {
+                new FlightTrack("ATR423", 39045, 12932, 14000),
+                new FlightTrack("BOR725", 25045, 56804, 13000),
+                new FlightTrack("MTB927", 57045, 12560, 12000),
+                new FlightTrack("KLT229", 70045, 11456, 11000),
+            };
 
+
+            var FlightsInSeperation = new List<ITrack>()
+            {
+                new FlightTrack("MTB927", 57045, 12560, 12000),
+                new FlightTrack("KLT229", 70045, 11456, 11000),
+            };
+
+
+            var monitor = new ConsoleMonitor();
+
+            monitor.ShowAllFlightsInAirspace(Flights);
+            monitor.ShowSeparationCondition(FlightsInSeperation);
             
+
         }
     }
 }
---
+++ AirTraficMonitoring/Monitor/ConsoleMonitor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using AirTraficMonitoring.Track;
+
+namespace AirTraficMonitoring.Monitor
+{
+    public class ConsoleMonitor : IMonitor
+    {
+        public ConsoleMonitor()
+        {
+            
+        }
+
+        public void ShowAllFlightsInAirspace(IList<ITrack> tracks)
+        {
+            foreach (var track in tracks)
+            {
+                Console.WriteLine($"{track.Tag} X:{track.XPosition} Y:{track.YPosition}");
+            }
+        }
+
+        public void ShowSeparationCondition(List<ITrack> tracks)
+        {
+            Console.WriteLine($"SEPERATION: [{tracks[0].Tag}, {tracks[1].Tag}]");
+        }
+    }
+}
