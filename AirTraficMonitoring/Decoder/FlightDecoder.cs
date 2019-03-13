c2f9a8779a0fd797bc4d6c6747d1088943a0cd02
bro <mikb182@gmail.com>
2019-03-13 14:09:20 +0100

    added decoder

---
+++ AirTraficMonitoring/Decoder/FlightDecoder.cs
@@ -0,0 +1,24 @@
+﻿using AirTraficMonitoring.Track;
+using System;
+using TransponderReceiver;
+
+namespace AirTraficMonitoring.Decoder
+{
+    public class FlightDecoder : IDecoder
+    {
+        public static ITransponderReceiver receiver;
+        private Track.FlightTrack newTrack;
+        public void DecoderEventHandler(object sender, RawTransponderDataEventArgs e)
+        {
+            foreach (var data in e.TransponderData)
+            {
+                var Seperated = data.Split(';');
+
+                newTrack = new FlightTrack(Seperated[0], Convert.ToDouble(Seperated[1]), Convert.ToDouble(Seperated[2]), Convert.ToDouble(Seperated[3]), Seperated[4]);
+            }
+            
+        }
+
+
+    }
+}
