c680da3d0767fbcb10d33f25001bc89c5153d3f5
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-03-13 11:09:22 +0100

    Minor changes

--- AirTraficMonitoring/FlightValidation/FlightValidation.cs
+++ AirTraficMonitoring/FlightValidation/FlightValidation.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Services;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AirTraficMonitoring
 {
-    public class FlightValidation : IFlightValidation
+    public class FlightValidation : Airspace
     {
-        public void validation()
+        public bool validation()
         {
-            
+            if (tesTrack.x <= Airspace.x && tesTrack.y <= Airspace.y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
