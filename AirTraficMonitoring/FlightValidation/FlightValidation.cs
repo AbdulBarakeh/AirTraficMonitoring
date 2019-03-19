e3e42cd16895a78c5c059ca49e9aa8ad5a85cfa5
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-03-19 13:50:17 +0100

    Expanded if statement

--- AirTraficMonitoring/FlightValidation/FlightValidation.cs
+++ AirTraficMonitoring/FlightValidation/FlightValidation.cs
@@ -11,20 +11,47 @@ namespace AirTraficMonitoring
 {
     namespace AirTraficMonitoring
     {
-        public class FlightValidation : IAirspace
+        public class FlightValidation
         {
-            public bool validation()
+
+
+            //public bool validation(IAirspace airSpace, ITrack track)
+            //{
+
+            //    if (((track.XPosition <= airSpace.Width) && (track.YPosition <= airSpace.Height)) &&
+            //         ((track.Altitude <= airSpace.MaxAlt) && (track.Altitude => airSpace.MinAlt))
+            //    {
+            //        return true;
+            //    }
+            //else
+            //{
+            //        return false;
+            //}
+
+            //}
+            public bool Validation(IAirspace airspace, ITrack track)
             {
-                if (ITrack.XPosition <= IAirspace.Width && ITrack.YPosition <= IAirspace.Height &&
-                    ITrack.Altitude <= IAirspace.maxAlt && ITrack.Altitude => IAirspace.minAlt)
-                {
-                    return true;
-                }
-                else
+                if (airspace.Width > track.XPosition)
                 {
                     return false;
                 }
 
+                if (airspace.Height > track.YPosition)
+                {
+                    return false;
+                }
+
+                if (track.Altitude > airspace.MaxAlt)
+                {
+                    return false;
+                }
+
+                if (track.Altitude < airspace.MinAlt)
+                {
+                    return true;
+                }
+
+                
             }
         }
     }
