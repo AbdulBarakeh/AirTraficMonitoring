91414fdd58826b0f22160c06846a590053f9b03d
bro <mikb182@gmail.com>
2019-03-20 21:36:08 +0100

    ..

--- AirTraficMonitoring.Application/Program.cs
+++ AirTraficMonitoring.Application/Program.cs
@@ -11,23 +11,15 @@ namespace AirTraficMonitoring.Application
     {
         public static void Main(string[] args)
         {
-            //var airspace = new Airspace(80000, 80000, 100, 20000);
-            //var decoder = new FlightDecoder(airspace, TransponderReceiverFactory.CreateTransponderDataReceiver());
+            var airspace = new Airspace(80000, 80000, 100, 20000);
+            var decoder = new FlightDecoder(airspace, TransponderReceiverFactory.CreateTransponderDataReceiver());
 
-            //var separation = new FlightSeparation(airspace);
+            var separation = new FlightSeparation(airspace);
 
-            //while (true)
-            //{
-            //    Thread.Sleep(1000);
-            //}
-
-
-            uint i = 100;
-            uint u = 200;
-
-            i -= u;
-
-            Console.WriteLine($"{i}");
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
         }
     }
 }
