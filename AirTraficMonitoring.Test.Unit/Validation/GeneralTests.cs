120b03a492bb64b5778364b9117c1bab99626eee
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-03-22 11:21:54 +0100

    Added four test cases

--- AirTraficMonitoring.Test.Unit/Validation/GeneralTests.cs
+++ AirTraficMonitoring.Test.Unit/Validation/GeneralTests.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using AirTraficMonitoring.FlightAirspace;
 using AirTraficMonitoring.Track;
 using AirTraficMonitoring.Validator;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace AirTraficMonitoring.Test.Unit.Validation
 {
@@ -16,34 +18,53 @@ namespace AirTraficMonitoring.Test.Unit.Validation
 
     public class GeneralTests
     {
+        
         private Validator.FlightValidator _vUUT;
         private Track.FlightTrack _testTrack;
         private FlightAirspace.Airspace _testAirspace;
+        
+        [SetUp]
 
         public void Setup()
         {
             _testAirspace = new FlightAirspace.Airspace(80000, 80000, 500, 20000);
-            _testTrack = new FlightTrack("au589", 78000, 55000, 15000, "20190322093230000");
-
-            _vUUT = new FlightValidator();
+            _vUUT = new Validator.FlightValidator();
         }
 
         [Test]
 
         public void TestingValidateCorrectData()
         {
-            var testTrack1 = new FlightTrack("au555", 78000, 55000, 15000, "20190322093230000");
+            var testTrack1 = new Track.FlightTrack("au555", 78000, 55000, 15000, "20151006213456785");
             bool validationValue = _vUUT.Validate(_testAirspace, testTrack1);
-            Assert.That(validationValue, Is.EqualTo(true));
+            Assert.That(validationValue, Is.True);
+        }
+            
+        [Test]
+
+        public void TestingValidateWrongData()
+        {
+            var testTrack2 = new FlightTrack("auC556", 81000, 81000, 21000, "20151006213456783");
+            bool validationValue = _vUUT.Validate(_testAirspace, testTrack2);
+            Assert.That(validationValue, Is.False);
         }
 
         [Test]
-        public void TestingValidateWrongData()
+
+        public void TestingNegativeXPosData()
         {
-            var testTrack2 = new FlightTrack("auC556", 81000, 81000, 21000, "20190322093230001");
-            bool validationValue1 = _vUUT.Validate(_testAirspace, testTrack2);
-            Assert.That(validationValue1, Is.EqualTo(false));
+            var testTrack3 = new FlightTrack("down555", -500, 30000, 5000, "20151006213456783");
+            bool validationValue = _vUUT.Validate(_testAirspace, testTrack3);
+            Assert.That(validationValue, Is.False);
         }
 
+        [Test]
+
+        public void TestingNegativeYPosData()
+        {
+            var testTrack3 = new FlightTrack("down555", 25000, -12, 5000, "20151006213456783");
+            bool validationValue = _vUUT.Validate(_testAirspace, testTrack3);
+            Assert.That(validationValue, Is.False);
+        }
     }
 }
--- AirTraficMonitoring/Validator/FlightValidator.cs
+++ AirTraficMonitoring/Validator/FlightValidator.cs
@@ -6,32 +6,15 @@ namespace AirTraficMonitoring.Validator
     public class FlightValidator : IValidator
     {
 
-        #region Comment
-
-        //public bool validation(IAirspace airSpace, ITrack track)
-        //{
-
-        //    if (((track.XPosition <= airSpace.Width) && (track.YPosition <= airSpace.Height)) &&
-        //         ((track.Altitude <= airSpace.MaxAlt) && (track.Altitude => airSpace.MinAlt))
-        //    {
-        //        return true;
-        //    }
-        //else
-        //{
-        //        return false;
-        //}
-
-        //} 
-        #endregion
 
         public bool Validate(IAirspace airspace, ITrack track)
         {
-            if (0 <= track.XPosition && track.XPosition >= airspace.Width)
+            if (track.XPosition <= 0 || track.XPosition >= airspace.Width)
             {
                 return false;
             }
 
-            if (0 <= track.YPosition && track.YPosition >= airspace.Height)
+            if (track.YPosition <= 0 || track.YPosition >= airspace.Height)
             {
                 return false;
             }
