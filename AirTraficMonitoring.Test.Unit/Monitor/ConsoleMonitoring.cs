2850080eec665fbc3aa717fa7bba46b3815749c1
Mads Steiner Kristensen <steiner.au@gmail.com>
2019-03-24 16:39:14 +0100

    Added test of events in ConsoleMonitor

--- AirTraficMonitoring.Test.Unit/Monitor/ConsoleMonitoring.cs
+++ AirTraficMonitoring.Test.Unit/Monitor/ConsoleMonitoring.cs
@@ -20,17 +20,30 @@ namespace AirTraficMonitoring.Test.Unit.Monitor
         private IAirspace _airspace;
         private ISeparation _separation;
 
+        private bool _eventHandled;
+        private FlightAddedEventArg _flightAddedEventArg;
+        private SeparationWarningEventArg _separationWarningEventArg;
+
         [SetUp]
         public void Setup()
         {
+            _eventHandled = false;
+
             _console = Substitute.For<ILog>();
             _airspace = Substitute.For<IAirspace>();
             _separation = Substitute.For<ISeparation>();
 
             _uut = new ConsoleMonitor(_console, _airspace, _separation);
+
+            _airspace.FlightAddedEvent += (sender, args) => _flightAddedEventArg = args;
+            _airspace.FlightAddedEvent += (sender, args) => _eventHandled = true;
+
+            _separation.SeparationWarningEvent += (sender, args) => _separationWarningEventArg = args;
+            _separation.SeparationWarningEvent += (sender, args) => _eventHandled = true;
         }
 
-
+        #region ShowFlightsMethod
+        
         [Test]
         public void ShowAllFlights_ListOfTracksIsEmpty_ConsoleLogInformationNotCalled()
         {
@@ -111,6 +124,9 @@ namespace AirTraficMonitoring.Test.Unit.Monitor
             _console.Received().LogError(Arg.Any<string>());
         }
 
+        #endregion
+
+        #region ShowSeparationMethod
 
         [Test]
         public void ShowSeparationCondition_ListOfTracksContainsZeroElements_ConsoleLogInformationNotCalled()
@@ -187,6 +203,75 @@ namespace AirTraficMonitoring.Test.Unit.Monitor
 
             _console.DidNotReceive().LogError(Arg.Any<string>());
         }
+
+
+        #endregion
+
+        #region FlightAddedEventHandling
+
+        [Test]
+        public void HandleFlightAddedEvent_FlightsReceived_EventIsHandled()
+        {
+            var flightTracks = new List<ITrack>()
+            {
+                TrackFactory.CreateTestTrack(),
+                TrackFactory.CreateTestTrack()
+            };
+
+            _airspace.FlightAddedEvent += Raise.EventWith(new FlightAddedEventArg(){ Tracks = flightTracks});
+
+            Assert.That(_eventHandled, Is.True);
+        }
+
+        [Test]
+        public void HandleFlightAddedEvent_FlightsReceived_ListOfFlightReceived()
+        {
+            var flightTracks = new List<ITrack>()
+            {
+                TrackFactory.CreateTestTrack(),
+                TrackFactory.CreateTestTrack()
+            };
+
+            _airspace.FlightAddedEvent += Raise.EventWith(new FlightAddedEventArg() {Tracks = flightTracks});
+
+            Assert.That(_flightAddedEventArg.Tracks, Is.EqualTo(flightTracks));
+        }
+
+        #endregion
+
+        #region SeparationWarningEventHandling
+
+        [Test]
+        public void HandleSeparationWarningEvent_SeparationOccured_EventIsHandled()
+        {
+            var flightTracks = new List<ITrack>()
+            {
+                TrackFactory.CreateTestTrack(),
+                TrackFactory.CreateTestTrack()
+            };
+
+            _separation.SeparationWarningEvent += Raise.EventWith(new SeparationWarningEventArg()
+                {SeparationList = flightTracks});
+                
+            Assert.That(_eventHandled, Is.True);
+        }
+
+        [Test]
+        public void HandleSeparationWarningEvent_SeparationOccured_ListOfFlightsReceived()
+        {
+            var flightTracks = new List<ITrack>()
+            {
+                TrackFactory.CreateTestTrack(),
+                TrackFactory.CreateTestTrack()
+            };
+
+            _separation.SeparationWarningEvent += Raise.EventWith(new SeparationWarningEventArg()
+                {SeparationList = flightTracks});
+
+            Assert.That(_separationWarningEventArg.SeparationList, Is.EqualTo(flightTracks));
+        }
+
+        #endregion
 
     }
 }
