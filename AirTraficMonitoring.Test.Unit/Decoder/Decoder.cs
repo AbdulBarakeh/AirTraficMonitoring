8e9b5597414690cd100db46b3591da620c9a1a6e
bro <mikb182@gmail.com>
2019-03-24 01:03:34 +0100

    kommet videre med decoder tests. dog ikke færdig endnu.

--- AirTraficMonitoring.Test.Unit/Decoder/Decoder.cs
+++ AirTraficMonitoring.Test.Unit/Decoder/Decoder.cs
@@ -12,14 +12,6 @@ using AirTraficMonitoring.Track;
 using AirTraficMonitoring.Decoder;
 using TransponderReceiver;
 
-//namespace AirTraficMonitoring.Test.Unit.Decoder
-//{
-//    public class Decoder
-//    {
-
-//    }
-//}
-
 namespace AirTraficMonitoring.Test.Unit.Decoder
 {
     [Author("MB")]
@@ -30,9 +22,7 @@ namespace AirTraficMonitoring.Test.Unit.Decoder
         private ITransponderReceiver _receiver;
         private IAirspace _airspace;
         private RawTransponderDataEventArgs _receiverEventArg;
-        private List<String> myList = new List<String>();
-
-        public object TrackFactory { get; private set; }
+        private List<String> _myList = new List<String>();
 
         [SetUp]
         public void Setup()
@@ -40,15 +30,56 @@ namespace AirTraficMonitoring.Test.Unit.Decoder
             _airspace = Substitute.For<IAirspace>();
             _receiver = Substitute.For<ITransponderReceiver>();
             _uut = new FlightDecoder(_airspace, _receiver);
+            _receiverEventArg = new RawTransponderDataEventArgs(_myList);
         }
 
         [Test]
-        public void FlightAdded_ListIsNotEmpty_()
+        public void Separator_ListIsEmpty()
         {
-            string newTrack = "0";
-             myList.Add(newTrack);
-            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: myList));
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: _myList));
             Assert.That(_receiverEventArg.TransponderData, Is.Empty);
+        }
+
+        [Test]
+        public void Separator_ListIsNotEmpty()
+        {
+            string newTrack = "st;10;10;10;10230";
+            _myList.Add(newTrack);
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: _myList));
+            Assert.That(_receiverEventArg.TransponderData, Is.Not.Empty);
+        }
+
+        [Test]
+        public void Separator_IsEqualTo()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: _myList));
+            Assert.That(_myList[0], Is.EqualTo("st"));
+            //Assert.That(_myList[1], Is.EqualTo(10));
+            //Assert.That(_myList[2], Is.EqualTo(10));
+            //Assert.That(_myList[3], Is.EqualTo(10));
+            //Assert.That(_myList[4], Is.EqualTo("10230"));
+        }
+
+        [Test]
+        public void FlightAdded_IsNotEqualTo_LowValue()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: _myList));
+            Assert.That(_myList[0], Is.Not.EqualTo("sp"));
+            Assert.That(_myList[0], Is.Not.EqualTo(9));
+            Assert.That(_myList[0], Is.Not.EqualTo(9));
+            Assert.That(_myList[0], Is.Not.EqualTo(9));
+            Assert.That(_myList[0], Is.Not.EqualTo("10229"));
+        }
+
+        [Test]
+        public void FlightAdded_IsNotEqualTo_HighValue()
+        {
+            _receiver.TransponderDataReady += Raise.EventWith(new RawTransponderDataEventArgs(transponderData: _myList));
+            Assert.That(_myList[0], Is.Not.EqualTo("at"));
+            Assert.That(_myList[0], Is.Not.EqualTo(11));
+            Assert.That(_myList[0], Is.Not.EqualTo(11));
+            Assert.That(_myList[0], Is.Not.EqualTo(11));
+            Assert.That(_myList[0], Is.Not.EqualTo("10231"));
         }
     }
 }
